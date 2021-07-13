978774c0b02f55513be9040714db61b7966af2dc
JayGhiya <ghiya6548@gmail.com>
2021-07-13 17:18:26 +0530

    Update TcpTransform.java

--- src/main/java/com/example/tcpreader/transform/TcpTransform.java
+++ src/main/java/com/example/tcpreader/transform/TcpTransform.java
@@ -3,6 +3,7 @@ package com.example.tcpreader.transform;
 import org.apache.flink.api.common.functions.MapFunction;
 
 public class TcpTransform implements MapFunction<String,String> {
+//Utilize the function to convert String to hl7 and modify the operator accordingly
     @Override
     public String map(String s) throws Exception {
         System.out.println("Message is:"+s);
